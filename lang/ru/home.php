41f8890d76cb3485345dc68bbc591863f4c65cb4
AlexeyGoncharenko <zomorc@gmail.com>
2024-05-08 20:03:49 +0300

    del

--- lang/ru/home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'home' => [
-        'description' => 'Это простой менеджер задач на Laravel',
-        'header' => 'Привет от Хекслета!',
-        'learn_more' => 'Нажми меня'
-    ]
-];
