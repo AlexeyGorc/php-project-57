a2ba2bf87c0282d1ac139cc6b6a7f7cbfd6503c3
AlexeyGoncharenko <zomorc@gmail.com>
2024-05-08 10:40:53 +0300

    step 2

---
+++ lang/ru/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'app_name' => 'Менеджер задач',
+    'name' => 'Имя',
+    'email' => 'Email',
+    'password' => 'Пароль',
+    'confirm_password' => 'Подтверждение',
+    'already_registered' => 'Уже зарегистрированы?',
+    'register_button' => 'Зарегистрировать',
+    'remember_me' => 'Запомнить меня',
+    'log_in' => 'Войти',
+    'forgot_your_password' => 'Забыли пароль?',
+    'email_password_reset_link' => 'Сбросить пароль',
+    'password_min' => 'Пароль должен иметь длину не менее :min символов',
+    'confirm_password_error' => 'Пароль и подтверждение не совпадают',
+    'failed' => 'Введите правильные имя пользователя и пароль',
+    'password_failed' => 'Неверный пароль',
+    'throttle' => 'Много попыток. Попробуйте через :seconds секунд.'
+];
